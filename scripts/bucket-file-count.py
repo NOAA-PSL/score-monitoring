24581ad0e93fb39273c4d31cba5c7bf8823670f9
Jessica Knezha <jessica.knezha@noaa.gov>
2023-02-03 16:45:28 -0700

    add bucket file count script

---
+++ scripts/bucket-file-count.py
@@ -0,0 +1,24 @@
+import sys
+import boto3
+from botocore import UNSIGNED
+from botocore.client import Config
+
+print("Arg value: ")
+print(sys.argv[1])
+
+year = "1998"
+month = "02"
+datetime = "1998022106"
+
+s3 = boto3.resource(
+    's3',
+    aws_access_key_id='',
+    aws_secret_access_key='',
+    config=Config(signature_version=UNSIGNED)
+)
+bucket = s3.Bucket('noaa-ufs-gefsv13replay-pds')
+
+prefix = "spinup/" + year + "/" + month + "/" + datetime + "/"
+file_count = bucket.objects.filter(Prefix=prefix).len()
+print("File count: ")
+print(file_count)
