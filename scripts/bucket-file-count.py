7f0786286d1a287027018b82cbd5213e3d231042
Jessica Knezha <jessica.knezha@noaa.gov>
2023-02-13 16:52:18 +0000

    in progress checking time difference is greater than 30 min, not currently running.

--- scripts/bucket-file-count.py
+++ scripts/bucket-file-count.py
@@ -1,6 +1,6 @@
 import sys
 import boto3
-from datetime import datetime
+import datetime as dt
 from botocore import UNSIGNED
 from botocore.client import Config
 
@@ -8,10 +8,10 @@ print("Arg value: ")
 print(sys.argv[1])
 
 input = sys.argv[1]
-datetime_obj =  datetime.strptime(input, "%Y%m%dT%H")
+datetime_obj = dt.datetime.strptime(input, "%Y%m%dT%H")
 year = datetime_obj.strftime("%Y")
 month = datetime_obj.strftime("%m")
-datetime = datetime_obj.strftime("%Y%m%d%H")
+datetime_str = datetime_obj.strftime("%Y%m%d%H")
 
 s3 = boto3.resource(
     's3',
@@ -19,18 +19,30 @@ s3 = boto3.resource(
     aws_secret_access_key='',
     config=Config(signature_version=UNSIGNED)
 )
-# bucket = s3.Bucket('noaa-ufs-gefsv13replay-pds')
-bucket = s3.Bucket('noaa-reanalyses-pds')
-
-# prefix = "spinup/" + year + "/" + month + "/" + datetime + "/"
-prefix = "observations/reanalysis/ozone/nasa/sbuv_v87/" + year + "/" +  month + "/bufr/"
+bucket = s3.Bucket('noaa-ufs-gefsv13replay-pds')
+# bucket = s3.Bucket('noaa-reanalyses-pds')
+prefix = "spinup/" + year + "/" + month + "/" + datetime_str + "/"
+# prefix = "observations/reanalysis/ozone/nasa/sbuv_v87/" + year + "/" +  month + "/bufr/"
 file_count = 0
+latest = dt.datetime(1, 1, 1, tzinfo=dt.timezone.utc)
 files = bucket.objects.filter(Prefix=prefix)
 for file in files: 
     file_count+=1
+    if latest < file.last_modified:
+        latest = file.last_modified
 
 if file_count is 0:
     raise Exception("no files found in bucket " + datetime)
+
+diff = dt.datetime.now(dt.timezone.utc) - latest 
+
+print("minutes")
+print(diff.minute) 
+if diff.minute < 30:
+    raise Exception("the latest file is more recent than 30 minutes, try again later")
+
+print("last modified:")
+print(latest)
 
 print("File count: ")
 print(file_count)
