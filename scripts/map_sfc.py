e5b3ba9a872853736420b6f8fc8e7d757dc371ea
Adam Schneider <adam.schneider@noaa.gov>
2026-05-07 17:07:27 -0600

    datetime bugfix 2

--- scripts/map_sfc.py
+++ scripts/map_sfc.py
@@ -7,6 +7,7 @@ optionally accumulate averages, and render surface radiation maps.
 import sys
 import shutil
 from datetime import datetime
+from datetime import timedelta
 from dotenv import load_dotenv
 import os
 import pathlib
@@ -208,7 +209,7 @@ class SurfaceMapper(object):
         effective_dt = self.datetime_obj
         
         if replay_offset not in [None, '']:
-            effective_dt = self.datetime_obj + datetime.timedelta(hours = int(replay_offset))
+            effective_dt = self.datetime_obj + timedelta(hours = int(replay_offset))
 
         if ref_file_name_nh is None:
             self.do_nh_sea_ice = False
